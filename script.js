210a0a7991ea6e11d45fcdcae35e27fd629df009
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-04 21:39:11 -0400

    Clear temp board array

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const gameBoard = (() => {
     // Store gameboard inside an array
     let board = [
         '','','',
-        'O','O','O',
-        'X','X','X'
+        '','','',
+        '','',''
     ];
 
     let renderBoard = () => {
