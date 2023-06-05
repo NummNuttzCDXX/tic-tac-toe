ac9274ac3bd414ee91241d96bc20d0a4326cb55b
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-04 20:06:02 -0400

    Add a wincheck and alert who wins

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Tic-Tac-Toe Script
 // Global Variable
-const spaces = document.querySelectorAll('.space');
+const spaces = document.querySelectorAll('.space')
+let spaceNum = []
+// Get data attr from spaces
+spaces.forEach((space) => {spaceNum.push(space.getAttribute('data'))})
 
 // Gameboard Object Module
 const gameBoard = (() => {
@@ -43,9 +46,65 @@ const Player = (name, marker) => {
 
 // Game Logic Object Module
 const game = (() => {
+    let winCheck = () => {
+        if (gameBoard.board[0] === gameBoard.board[1] && gameBoard.board[1] === gameBoard.board[2]) { // Check top row
+            if (gameBoard.board[0] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[0] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[0] === gameBoard.board[3] && gameBoard.board[3] === gameBoard.board[6]) { // Check left side
+            if (gameBoard.board[0] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[0] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[0] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[8]) { // Check diagonal right
+            if (gameBoard.board[0] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[0] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[3] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[5]) { // Check middle row
+            if (gameBoard.board[3] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[3] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[6] === gameBoard.board[7] && gameBoard.board[7] === gameBoard.board[8]) { // Check bottom row
+            if (gameBoard.board[6] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[6] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[1] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[7]) { // Check middle column
+            if (gameBoard.board[1] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[1] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[2] === gameBoard.board[5] && gameBoard.board[5] === gameBoard.board[8]) { // Check right column
+            if (gameBoard.board[2] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[2] === player2.marker) {
+                gameOver(player2.name)
+            }
+        } else if (gameBoard.board[2] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[6]) { // Check diagonal left
+            if (gameBoard.board[2] === player1.marker) {
+                gameOver(player1.name)
+            } else if (gameBoard.board[2] === player2.marker) {
+                gameOver(player2.name)
+            }
+        }
+    }
 
+    let gameOver = (player) => {
+        alert(player + ' wins!!')
+    }
+
+    return {winCheck}
 })()
 
 // TEMP
 gameBoard.renderBoard()
-player1 = Player('Player 1', 'X')
+let player1 = Player('Player 1', 'X')
